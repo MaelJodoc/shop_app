5626edfef1ecb532178f867e162344659d0f6a98
mael <mael.jodoc@gmail.com>
2022-05-20 14:59:55 +0300

    11-12

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -18,7 +18,7 @@ class UserProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Products products = Provider.of<Products>(context)..fetchAndSetProducts();
+    //final Products products = Provider.of<Products>(context)..fetchAndSetProducts();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
@@ -33,29 +33,44 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshProducts(context),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListView.builder(
-            itemBuilder: (ctx, i) {
-              Product p = products.items[i];
-              return Column(
-                children: [
-                  UserProductItem(
-                    id: p.id,
-                    title: p.title,
-                    imageUrl: p.imageUrl,
+      body: FutureBuilder(
+        future: Provider.of<Products>(context, listen: false).fetchAndSetProducts(),
+        builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
+          return Consumer<Products>(
+            builder: (context, value, child) {
+              if (snapshot.connectionState==ConnectionState.done) {
+                return RefreshIndicator(
+                  onRefresh: () => _refreshProducts(context),
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: ListView.builder(
+                      itemBuilder: (ctx, i) {
+                        Product p = value.items[i];
+                        return Column(
+                          children: [
+                            UserProductItem(
+                              id: p.id,
+                              title: p.title,
+                              imageUrl: p.imageUrl,
+                            ),
+                            Divider(
+                              thickness: 1.5,
+                            ),
+                          ],
+                        );
+                      },
+                      itemCount: value.items.length,
+                    ),
                   ),
-                  Divider(
-                    thickness: 1.5,
-                  ),
-                ],
-              );
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
             },
-            itemCount: products.items.length,
-          ),
-        ),
+          );
+        },
       ),
     );
   }
